dcaf65f22f74833ff2c4ecbb5d02bc9547c6d49b
Pratik Singh <pratikkumars623@gmail.com>
2024-08-27 20:29:43 +0530

    Initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,13 @@ import SignUp from './Pages/SignUp/SignUp.component'
 import Step1 from './Pages/SignUp/step1/step1.component'
 import Steps from './Pages/SignUp/steps'
 import Farmer from './Pages/Farmer/Farmer.component'
+import LandingPage from './Pages/LandingPage'
 
 function App() {
 
     return (
         <Routes>
-            {/* <Route path="/" element={<LandingPage />} /> */}{/*Todo: Add Landing Page here */}
+            <Route path="/" element={<LandingPage />} /> 
             <Route path="/signup" element={<SignUp />} >
                 <Route index element={<Step1 />} />
                 <Route path=":id" element={<Steps />} />
