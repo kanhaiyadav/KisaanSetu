44c3a4cf1dff03c0426c8a139866065b2bf82a50
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-08-30 00:12:37 +0530

    little changes

--- client/src/Pages/Farmer/ProductCard.jsx
+++ client/src/Pages/Farmer/ProductCard.jsx
@@ -6,6 +6,7 @@ const ProductCard = ({ product }) => {
         <Card intent={'fitContent'} className="break-inside-avoid relative">
             <img src={image} alt={name} className="w-full object-cover rounded-t-xl" />
             <div className="pt-2 w-full">
+                
                 <h1 className="text-lg font-semibold text-gray-800">{name}</h1>
                 <p className="text-gray-500 text-2xl">₹{price}</p>
                 <div className="absolute bg-primary top-0 right-6 p-2 min-h-[50px] rounded-b-2xl">{remainingStock}</div>
