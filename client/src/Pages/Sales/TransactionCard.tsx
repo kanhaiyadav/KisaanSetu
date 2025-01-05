da1940796ac75daf8d389c4ed24e659c23066a20
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-01-05 21:23:29 +0530

    Integrating chatting

--- client/src/Pages/Sales/TransactionCard.tsx
+++ client/src/Pages/Sales/TransactionCard.tsx
@@ -16,20 +16,20 @@ const TransactionCard = ({ sale, bg, ...otherProps }: {
             <div className="flex items-center gap-2 sm:gap-4">
                 <Link
                     to={"../products"}
-                    className="text-md lg:text-lg font-semibold font-sans text-orange-600"
+                    className="text-md font-semibold font-sans text-orange-600 whitespace-nowrap"
                 >
                     {sale.productName}
                 </Link>
             </div>
             <div className="flex items-center gap-2 sm:gap-4">
-                <p className="text-xs lg:text-sm text-gray-700">
+                <p className="text-xs text-gray-700 whitespace-nowrap">
                     Quantity:{" "}
                     <span className="font-semibold text-gray-800">
                         {sale.quantity}
                         {sale.stocksUnit}
                     </span>
                 </p>
-                <p className="text-xs lg:text-sm text-gray-700">
+                <p className="text-xs text-gray-700 whitespace-nowrap">
                     Total:{" "}
                     <span className="font-semibold text-gray-800">
                         ₹{sale.total}
@@ -37,12 +37,12 @@ const TransactionCard = ({ sale, bg, ...otherProps }: {
                 </p>
             </div>
             <div className="flex items-center gap-2 sm:gap-4">
-                <p className="text-xs lg:text-sm text-gray-700">
+                <p className="text-xs text-gray-700 whitespace-nowrap">
                     <span className="font-semibold text-gray-800">
                         {sale.customer}
                     </span>
                 </p>
-                <p className="text-xs lg:text-sm text-gray-700">
+                <p className="text-xs text-gray-700 whitespace-nowrap">
                     {sale.date.split("T")[0]}
                 </p>
             </div>
