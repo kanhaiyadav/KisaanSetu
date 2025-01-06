4474e04531d850fa22e08a2ffc8616084bba8c42
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-01-06 09:04:15 +0530

    change classification technique from passing file path to file data directly to the image classifier model

--- classifier/app.py
+++ classifier/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ultralytics import YOLO
 from werkzeug.utils import secure_filename
-import os
+from io import BytesIO
+from PIL import Image
+import numpy as np
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -11,22 +13,26 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_cred
 # Load YOLO model
 model = YOLO('./best.pt')
 
-# Configure upload folder
-UPLOAD_FOLDER = './uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 # Classify image function
 def classify(img_file):
-    results = model(img_file)  # Use the image file directly
-    names = results[0].names
-    probs = results[0].probs
+    try:
+         # Open the image file
+        image = Image.open(img_file)
 
-    name = names[probs.top1]
-    conf = float(probs.top1conf)
-    if conf < 0.5:
-        return {"status": "Fail", "confidence": conf}
-    return {"name": name, "confidence": conf}
+         # Convert the image to a format YOLO expects (NumPy array)
+        img_array = np.array(image)
+        
+        results = model(img_array)  # Use the image file directly
+        names = results[0].names
+        probs = results[0].probs
+
+        name = names[probs.top1]
+        conf = float(probs.top1conf)
+        if conf < 0.5:
+            return {"status": "Fail", "confidence": conf}
+        return {"name": name, "confidence": conf}
+    except Exception as e:
+        return {"error": f"Unsupported image type or processing error: {str(e)}"}
 
 # Route for image classification
 @app.route('/api/classify', methods=['POST'])
@@ -38,21 +44,14 @@ def classify_image():
     if img_file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
-    # Save the image securely
-    filename = secure_filename(img_file.filename)
-    img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    img_file.save(img_path)
-
     # Perform classification
     try:
-        result = classify(img_path)
-        # os.remove(img_path)
+        img_data = BytesIO(img_file.read())
+        result = classify(img_data)
         return jsonify(result), 200
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
-    finally:
-        # Remove the image file
-        os.remove(img_path)
+        print(e)
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
